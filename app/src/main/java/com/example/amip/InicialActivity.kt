ec1350fb732ad25cff20f6b6c06abb2737bacc52
Filipe <filipesgama@gmail.com>
2024-11-05 14:16:59 -0300

    novo

--- app/src/main/java/com/example/amip/InicialActivity.kt
+++ app/src/main/java/com/example/amip/InicialActivity.kt
@@ -8,28 +8,28 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.ola_mundo.R
+import com.example.ola_mundo.R.*
 
 class InicialActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_inicial)
+        setContentView(layout.activity_inicial)
 
-        // Ajusta as margens para exibir a imagem adequadamente com as barras do sistema
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-        // Referência ao botão no layout
-        val button = findViewById<Button>(R.id.bt_recenseador)
+        val button = findViewById<Button>(id.bt_recenseador)
+        val button2 = findViewById<Button>(id.bt_administrador)
 
-        // Ao clicar no botão, inicia a SecondActivity
         button.setOnClickListener {
-            // Cria uma Intent explícita para chamar a SecondActivity
             val intent = Intent(this, LoginRecenseadorActivity::class.java)
-
-            // Inicia a SecondActivity
+            startActivity(intent)
+        }
+        button2.setOnClickListener {
+            val intent = Intent(this, InicialAdministradorActivity::class.java)
             startActivity(intent)
         }
     }
