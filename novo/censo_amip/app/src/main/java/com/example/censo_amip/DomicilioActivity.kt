35e3a03511fcd9a4ae640433ad7afd264a0f9efe
César Viana Barros <126491073+CVbarro@users.noreply.github.com>
2024-11-21 22:06:30 -0300

    Scanner feito

--- novo/censo_amip/app/src/main/java/com/example/censo_amip/DomicilioActivity.kt
+++ novo/censo_amip/app/src/main/java/com/example/censo_amip/DomicilioActivity.kt
@@ -3,18 +3,63 @@ package com.example.censo_amip
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class DomicilioActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_domicilio)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val spinnerRua: Spinner = findViewById(R.id.spinnerRua)
+        val spinnerMoradia: Spinner = findViewById(R.id.spinnerMoradia)
+        val spinnerTipoMoradia: Spinner = findViewById(R.id.spinnerTipoMoradia)
+
+        val ruas = listOf(
+            "Marina das Estrelas",
+            "Marina das Flores",
+            "Marina dos Bosques",
+            "Marina Porto Bali",
+            "Marina das Hortênsias",
+            "Marina Ponta Leste",
+            "Marina da Lua",
+            "Marina do Frade",
+            "Marina do Sol",
+            "Marina dos Coqueiros",
+            "Canalzinho"
+        )
+
+        val moradias = listOf(
+            "Sim",
+            "Nao"
+        )
+
+        val tipoMoradias = listOf(
+            "Casa própria",
+            "Casa alugada",
+            "Casa cedida",
+            "Apartamento próprio",
+            "Apartamento alugado",
+            "Apartamento cedido",
+            "Barraco",
+            "Chácara/Sítio"
+
+        )
+
+        val ruasadapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, ruas)
+        ruasadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val moradiasadapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, moradias)
+        moradiasadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        val tipoMoradiaadapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, tipoMoradias)
+        tipoMoradiaadapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerMoradia.adapter = ruasadapter
+        spinnerRua.adapter = moradiasadapter
+        spinnerTipoMoradia.adapter = tipoMoradiaadapter
+
+
+
+
     }
-}
+    }
